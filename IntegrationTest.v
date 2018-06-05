000e68346a70d279a4e78c72e2e9818c9cf94d8b
pbzweihander <sd852456@naver.com>
2018-06-05 16:36:26 +0900

    fourth day...

--- IntegrationTest.v
+++ IntegrationTest.v
@@ -5,51 +5,50 @@ module IntegrationTest;
 	reg reset;
 	reg [7:0] instruction;
 	wire [7:0] pc;
-	wire [6:0] write_data_display_low;
-	wire [6:0] write_data_display_high;
+	wire [6:0] display_low;
+	wire [6:0] display_high;
 
-    wire [7:0] MemByte[20:0];
+	wire [7:0] MemByte[20:0];
 
-    assign MemByte[0] = 8'b01000100;
-    assign MemByte[1] = 8'b01001001;
-    assign MemByte[2] = 8'b00011001;
-    assign MemByte[3] = 8'b10000100;
+	assign MemByte[0] = 8'b01000100;
+	assign MemByte[1] = 8'b01001001;
+	assign MemByte[2] = 8'b00011001;
+	assign MemByte[3] = 8'b10000100;
 
-    assign MemByte[4] = 8'b01000100;
-    assign MemByte[5] = 8'b01001001;
-    assign MemByte[6] = 8'b00011001;
-    assign MemByte[7] = 8'b10000100;
+	assign MemByte[4] = 8'b01000100;
+	assign MemByte[5] = 8'b01001001;
+	assign MemByte[6] = 8'b00011001;
+	assign MemByte[7] = 8'b10000100;
 
-    assign MemByte[8] = 8'b01000100;
-    assign MemByte[9] = 8'b01001001;
-    assign MemByte[10] = 8'b00011001;
-    assign MemByte[11] = 8'b10000100;
+	assign MemByte[8] = 8'b01000100;
+	assign MemByte[9] = 8'b01001001;
+	assign MemByte[10] = 8'b00011001;
+	assign MemByte[11] = 8'b10000100;
 
-    assign MemByte[12] = 8'b01000100;
-    assign MemByte[13] = 8'b01001001;
-    assign MemByte[14] = 8'b00011001;
-    assign MemByte[15] = 8'b10000100;
+	assign MemByte[12] = 8'b01000100;
+	assign MemByte[13] = 8'b01001001;
+	assign MemByte[14] = 8'b00011001;
+	assign MemByte[15] = 8'b10000100;
 
-    assign MemByte[16] = 8'b01000100;
-    assign MemByte[17] = 8'b01001001;
-    assign MemByte[18] = 8'b00011001;
-    assign MemByte[19] = 8'b10000100;
+	assign MemByte[16] = 8'b01000100;
+	assign MemByte[17] = 8'b01001001;
+	assign MemByte[18] = 8'b00011001;
+	assign MemByte[19] = 8'b10000100;
 
-    assign MemByte[20] = 8'b11000011;
-
-    assign instruction = MemByte[pc];
+	assign MemByte[20] = 8'b11000011;
 
 	Microprocessor uut (
 		.origclk(origclk),
 		.reset(reset),
 		.instruction(instruction),
 		.pc(pc),
-		.write_data_display_low(write_data_display_low),
-		.write_data_display_high(write_data_display_high)
+		.display_low(display_low),
+		.display_high(display_high)
 	);
 
 	initial begin
 		origclk = 0;
+		instruction = MemByte[0];
 		reset = 0;
 		#10;
 		reset = 1;
@@ -62,5 +61,9 @@ module IntegrationTest;
 		#1;
 		origclk = 0;
 		#1;
+	end
+
+	always @(pc) begin
+		instruction <= MemByte[pc];
 	end
 endmodule
